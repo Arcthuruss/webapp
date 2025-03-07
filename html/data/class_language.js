50a3b151887bcdd12267788138419c5e054bef88
Arcthuruss <73853221+Arcthuruss@users.noreply.github.com>
2025-03-07 15:07:43 +0100

    class_languages.js

---
+++ html/data/class_language.js
@@ -0,0 +1,36 @@
+import { countries } from "./countries.js"
+
+class Language {
+
+    static all_languages = []
+
+
+    constructor(name, iso) {
+        this._name = name
+        this._iso = iso
+    }
+
+    get name() {return this._name}
+    set name(newName) {this._name = newName}
+
+    get iso() {return this._iso}
+    set iso(newIso) {this._iso = newIso}
+
+    toString() {
+        return `${this.name} (${this.iso})`
+    }
+
+    static fill_languages(source) {
+        source.forEach(country => {
+            country.languages.forEach(e => {
+                if (!(e.iso639_2 in this.all_languages)) {
+                    this.all_languages[e.iso639_2] = new Language(e.name, e.iso639_2)
+                }
+            })
+        })
+    }
+
+}
+
+Language.fill_languages(countries)
+console.log(Language.all_languages["fra"].toString())
